7e364c3caad02d34705e6d7d69599a128113a090
Akshay Thakur <thakurakshay8375@gmail.com>
2022-03-27 20:09:24 +0530

    removed routing error by installing react-router-dom version 5.2.0

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React,{Fragment} from 'react';
 import './App.css';
-import {BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import {BrowserRouter as Router, Route,Switch } from 'react-router-dom'
 import Navbar from './components/layout/Navbar';
 import About from './components/pages/About';
 import Home from './components/pages/Home';
@@ -11,15 +11,15 @@ const App=()=> {
     <Fragment className="App">
       <Navbar/>
       <div className='container'>
-        <Routes>
+        <Switch>
           <Route exact path='/' component={Home}/>
           <Route exact path='/about' component={About}/>
 
-        </Routes>
+        </Switch>
       </div>
     </Fragment>
     </Router>
-  );
+  )
 }
 
 export default App;
